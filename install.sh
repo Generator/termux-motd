8529c5328feb577900c260a783bd097f56df5916
Generator <american.jesus.pt@gmail.com>
2021-03-21 23:09:47 +0000

    Fix shebang Fix find default shell

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/data/data/com.termux/files/usr/bin/env bash
 
 git clone https://github.com/Generator/termux-motd.git $HOME/.motd
 
 [ -f $PREFIX/etc/motd ] && mv $PREFIX/etc/motd $PREFIX/etc/motd.orig
 
-[ $SHELL == "zsh" ] && _PROFILE=$HOME/.zprofile || _PROFILE=$HOME/.profile
+[ $(basename $SHELL) == "zsh" ] && _PROFILE=$HOME/.zprofile || _PROFILE=$HOME/.profile
 echo "$HOME/.motd/init.sh" >> $_PROFILE
