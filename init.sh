5c03c1684f1c97c82a7b24242514cb469557bf27
Generator <american.jesus.pt@gmail.com>
2021-03-22 15:32:28 +0000

    Don't rename original motd Clear original motd Fix duplicate init.sh on .profile

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/env bash
 
+clear
+
 [ -d "$HOME/motd" ] && MOTD="$HOME/motd"
 [ -d "$HOME/.motd" ] && MOTD="$HOME/.motd"
 [ -d "$PREFIX/motd" ] && MOTD="$PREFIX/motd"
-
-
 
 for i in $(find $MOTD -maxdepth 1 -regex '.+/[0-9\-].+' -not -regex ".+\.disabled" | sort)
 do
--- install.sh
+++ install.sh
@@ -2,7 +2,5 @@
 
 git clone https://github.com/Generator/termux-motd.git $HOME/.motd
 
-[ -f $PREFIX/etc/motd ] && mv $PREFIX/etc/motd $PREFIX/etc/motd.orig
-
 [ $(basename $SHELL) == "zsh" ] && _PROFILE=$HOME/.zprofile || _PROFILE=$HOME/.profile
-echo "$HOME/.motd/init.sh" >> $_PROFILE
+[ $(grep "$HOME/.motd/init.sh" $_PROFILE) != "0" ] && echo "$HOME/.motd/init.sh" >> $_PROFILE
